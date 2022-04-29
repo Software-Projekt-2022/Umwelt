e57e6c66d8a2e7aa763651c4ca2cb38b814edc93
tschnittger <64741047+tschnittger@users.noreply.github.com>
2022-04-29 14:23:20 +0200

    Fixed folder structure

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const port = 3000;
 app.get('/Umwelt', function(req, res){
     res.sendFile(path.join(__dirname,"index.html"));
 });
-app.use(express.static('Umwelt22'));
+app.use(express.static('src'));
 app.listen(port);
 
