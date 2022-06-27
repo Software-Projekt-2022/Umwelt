0afb055593639ddc73315b61c561a6ec57b0b664
tschnittger <64741047+tschnittger@users.noreply.github.com>
2022-06-27 22:21:02 +0200

    checkData now first checks if any data is provided

--- public/js/api/serverTools.js
+++ public/js/api/serverTools.js
@@ -1,6 +1,6 @@
 const fetching = require('./fetching');
 const evaluation = require('./evaluation');
-var warnings=[];
+var warnings = [];
 var weatherData;
 var histoData;
 var airData;
@@ -12,14 +12,15 @@ var allData = {};
  * This function is called when the server starts up/resets. It fetches the data, sends it to the frontend and evaluates it.
  * @param {*} app 
  */
-exports.startUp = async function(app) {
-    weatherData=await fetching.fetchWeather();
-    histoData=await fetching.fetchHistoricalWeather();
-    airData=await fetching.fetchAir();
-    riverData=await fetching.fetchRiver();
-    pollenData=await fetching.fetchPollen();
-    warnings = [];
-    if(checkData()){
+exports.startUp = async function (app) {
+    weatherData = await fetching.fetchWeather();
+    histoData = await fetching.fetchHistoricalWeather();
+    airData = await fetching.fetchAir();
+    riverData = await fetching.fetchRiver();
+    pollenData = await fetching.fetchPollen();
+    warnings.length = 0;
+
+    if (checkData()) {
         warnings.push(await evaluation.evaluateWeather(weatherData));
         warnings.push(await evaluation.evaluateAir(airData));
         warnings.push(await evaluation.evaluateRiver(riverData));
@@ -43,7 +44,7 @@ exports.startUp = async function(app) {
             }
         }
 
-        app.get('/api/getAllData', async(req, res) => {
+        app.get('/api/getAllData', async (req, res) => {
             res.send(allData)
         });
     }
@@ -53,14 +54,14 @@ exports.startUp = async function(app) {
  * This function is called to refresh the data. It fetches the data and sends it to the frontend.
  * @param {*} app 
  */
-exports.refreshData = async function(app) {
-    weatherData=await fetching.fetchWeather();
-    histoData=await fetching.fetchHistoricalWeather();
-    airData=await fetching.fetchAir();
-    riverData=await fetching.fetchRiver();
-    pollenData=await fetching.fetchPollen();
+exports.refreshData = async function (app) {
+    weatherData = await fetching.fetchWeather();
+    histoData = await fetching.fetchHistoricalWeather();
+    airData = await fetching.fetchAir();
+    riverData = await fetching.fetchRiver();
+    pollenData = await fetching.fetchPollen();
 
-    if(checkData()){
+    if (checkData()) {
         allData = {
             "weather": weatherData,
             "historicalWeather": histoData,
@@ -79,7 +80,7 @@ exports.refreshData = async function(app) {
             }
         }
 
-        app.get('/api/getAllData', async(req, res) => {
+        app.get('/api/getAllData', async (req, res) => {
             res.send(allData)
         });
     }
@@ -89,20 +90,24 @@ exports.refreshData = async function(app) {
  * Checks if the fetched data contains the first thing thats used in api-handler.js.
  * @returns true if the data is ok, false if not.
  */
-function checkData(){
-    if(weatherData.hourly==null || weatherData.hourly==undefined){
+function checkData() {
+    //Check if any of the data is null to prevent errors
+    if (weatherData == null || histoData == null || airData == null || riverData == null || pollenData == null) {
         return false;
     }
-    if(histoData[0].hourly == null){
+    if (weatherData.hourly == null || weatherData.hourly == undefined) {
         return false;
     }
-    if(airData.list==null){
+    if (histoData[0].hourly == null) {
         return false;
     }
-    if(riverData[0]==null){
+    if (airData.list == null) {
         return false;
     }
-    if(pollenData.pollen==null){
+    if (riverData[0] == null) {
+        return false;
+    }
+    if (pollenData.pollen == null) {
         return false;
     }
     return true;
